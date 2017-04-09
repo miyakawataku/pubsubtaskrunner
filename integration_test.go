51ebe9468676952ca9d23a6fd10e96d3cfd9cdfe
Miyakawa Taku <miyakawa.taku@gmail.com>
2017-04-09 15:12:11 +0900

    make integration test better

--- integration_test.go
+++ integration_test.go
@@ -1,7 +1,9 @@
+// +build integration
+
 package main
 
 import (
-	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -13,34 +15,7 @@ import (
 	"google.golang.org/api/option"
 )
 
-type e2eTestConf struct {
-	project string
-	topic   string
-	subs    string
-	cred    string
-}
-
-func getE2eTestConf() (*e2eTestConf, error) {
-	var err error
-	readEnv := func(envName string, p *string) {
-		*p = os.Getenv(envName)
-		if *p == "" {
-			err = errors.New(fmt.Sprintf("environment variable %s not provided", envName))
-		}
-	}
-	testConf := e2eTestConf{}
-	readEnv("PUBSUBTASKRUNNER_TESTS_PROJECT", &testConf.project)
-	readEnv("PUBSUBTASKRUNNER_TESTS_TOPIC", &testConf.topic)
-	readEnv("PUBSUBTASKRUNNER_TESTS_SUBS", &testConf.subs)
-	readEnv("PUBSUBTASKRUNNER_TESTS_CREDENTIALS", &testConf.cred)
-	if err != nil {
-		return nil, err
-	} else {
-		return &testConf, nil
-	}
-}
-
-func TestEnd2End(t *testing.T) {
+func TestIntegration(t *testing.T) {
 	tempDir, err := ioutil.TempDir("", "pubsubtaskrunnertest")
 	if err != nil {
 		t.Errorf("could not create a temp dir: %v", err)
@@ -48,30 +23,26 @@ func TestEnd2End(t *testing.T) {
 	}
 	defer os.RemoveAll(tempDir)
 
-	testConf, err := getE2eTestConf()
-	if err != nil {
-		t.Error(err)
-		return
-	}
-
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
-	psClient, err := pubsub.NewClient(ctx, testConf.project, option.WithServiceAccountFile(testConf.cred))
+	psClient, err := pubsub.NewClient(ctx, *project, option.WithServiceAccountFile(*credentials))
 	if err != nil {
 		t.Errorf("could not connect to Pub/Sub: %v", err)
 		return
 	}
-	topic := psClient.Topic(testConf.topic)
+	defer psClient.Close()
+
+	topic := psClient.Topic(*topic)
 	topic.Publish(ctx, &pubsub.Message{Data: []byte("foobar\n")})
 	topic.Publish(ctx, &pubsub.Message{Data: []byte("barfoo\n")})
 
 	conf := conf{
 		command:        "/bin/sed",
 		args:           []string{"s/foo/FOO/"},
-		project:        testConf.project,
-		subscription:   testConf.subs,
-		credentials:    testConf.cred,
+		project:        *project,
+		subscription:   *subscription,
+		credentials:    *credentials,
 		parallelism:    2,
 		tasklogdir:     tempDir,
 		maxtasklogkb:   1000,
@@ -89,4 +60,20 @@ func TestEnd2End(t *testing.T) {
 	if output != expected1 && output != expected2 {
 		t.Errorf("output of tasks must be %v or %v, but was %v", expected1, expected2, output)
 	}
+}
+
+var (
+	project      = flag.String("project", "", "GCP project ID")
+	topic        = flag.String("topic", "", "Pub/Sub topic ID")
+	subscription = flag.String("subscription", "", "Pub/Sub subscription ID")
+	credentials  = flag.String("credentials", "", "path to credentials JSON file")
+)
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+	if *project == "" || *topic == "" || *subscription == "" {
+		fmt.Fprintln(os.Stderr, "--project, --topic, --subscription flags are all required for integration test")
+		os.Exit(1)
+	}
+	os.Exit(m.Run())
 }
